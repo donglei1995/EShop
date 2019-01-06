3a30ef5137d98a32999f5a1ccf5196cc14f61e8b
donglei1995 <1960735821@qq.com>
2019-01-06 23:39:39 +0800

    ye

---
+++ src/main/java/com/base/BaseAction.java
@@ -0,0 +1,9 @@
+package com.base;
+import org.apache.log4j.Logger;
+import org.apache.struts.actions.DispatchAction;
+
+/** 具有日志功能的DispatchAction子类 */
+public class BaseAction extends DispatchAction {
+	/** 取得日志记录器Logger */
+	public static Logger logger = Logger.getLogger(BaseAction.class);
+}
